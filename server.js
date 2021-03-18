8e2ab7d2b3b371e6ad31050f5f3d9d04156e7ca9
Shoaib Ali <shoaibalichoudhary@gmail.com>
2021-03-19 00:19:54 +0530

    second

--- server.js
+++ server.js
@@ -4,4 +4,10 @@ const dotenv = require('dotenv')
 dotenv.config({ path: './config/config.env' })
 const app = express()
 const PORT = process.env.PORT || 5000
+
+app.get('/', (req, res) => {
+res.send('<h1>hello i want to contact you</h1> ')
+
+})
+
 app.listen(PORT,console.log(`this is run on PORT ${PORT} and in ${process.env.NODE_ENV}`))
